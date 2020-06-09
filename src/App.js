0962870a743ce3abc371a9b2ade360015dd7d211
Woojoung <chae3229@ipixelcorp.com>
2020-06-09 18:27:09 +0900

    Create fist React Component and make reusable coomponents with jsx + props

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
+// Reusable Components with JSX + Props
+// using props
+function Food({ fav }) {
+  return <h3>I Love {fav}</h3>;
+}
+
+const foodILike = [
+  {
+    name: "Kimchi",
+    image:
+      "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.koreanbapsang.com%2Fvegan-kimchi%2F&psig=AOvVaw2eVWjaIloAtBkqdGhYfmeS&ust=1591780418203000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCIitwKWy9OkCFQAAAAAdAAAAABAJ",
+  },
+  {},
+];
+
+// component : which is function to return html
+// JSX : html + javascript
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>HELLO!!! React!!</h1>
+      <Food fav="kimchi" />
+      <Food fav="ramen" />
+      <Food fav="Tofu" />
+      <Food fav="snack" />
+      <Food fav="kimchi" />
+      <Food fav="ramen" />
+      <Food fav="Tofu" />
+      <Food fav="snack" />
     </div>
   );
 }
