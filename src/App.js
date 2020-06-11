4daf58a52dc5dd701d2714a262347d88eb2f3072
Woojoung <chae3229@ipixelcorp.com>
2020-06-11 13:25:24 +0900

    Dynamic component Generation with map and object in react

--- src/App.js
+++ src/App.js
@@ -2,17 +2,43 @@ import React from "react";
 
 // Reusable Components with JSX + Props
 // using props
-function Food({ fav }) {
-  return <h3>I Love {fav}</h3>;
+function Food({ name, picture }) {
+  return (
+    <div>
+      <h3>I Love {name}</h3>
+      <img src={picture}></img>
+    </div>
+  );
 }
+// Add dynamic data to web (we dont have any data from the beginning)
+// imagine data came from the API
 
 const foodILike = [
   {
     name: "Kimchi",
     image:
-      "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.koreanbapsang.com%2Fvegan-kimchi%2F&psig=AOvVaw2eVWjaIloAtBkqdGhYfmeS&ust=1591780418203000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCIitwKWy9OkCFQAAAAAdAAAAABAJ",
+      "http://d3pah2c10lnl36.cloudfront.net/sura_wp/wp-content/uploads/2015/10/SUra-Korean-Cuisine-Koreas-Greatest-Food-Kimchi-Blog.jpg",
   },
-  {},
+  {
+    name: "Kimbap",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/Vegetable_gimbap.jpg/1200px-Vegetable_gimbap.jpg",
+  },
+  {
+    name: "Samgyeopsal",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/7/77/Samgyeopsal-gui.jpg/1200px-Samgyeopsal-gui.jpg",
+  },
+  {
+    name: "Bibimbap",
+    image:
+      "https://howtofeedaloon.com/wp-content/uploads/2016/02/bibimbap-feature-V21-1024x866.jpg",
+  },
+
+  {
+    name: "Donkasu",
+    image: "http://www.wako-group.co.jp/kr/images/index/about.jpg",
+  },
 ];
 
 // component : which is function to return html
@@ -21,14 +47,9 @@ function App() {
   return (
     <div>
       <h1>HELLO!!! React!!</h1>
-      <Food fav="kimchi" />
-      <Food fav="ramen" />
-      <Food fav="Tofu" />
-      <Food fav="snack" />
-      <Food fav="kimchi" />
-      <Food fav="ramen" />
-      <Food fav="Tofu" />
-      <Food fav="snack" />
+      {foodILike.map((dish) => (
+        <Food name={dish.name} picture={dish.image} /> // dish is just object
+      ))}
     </div>
   );
 }
