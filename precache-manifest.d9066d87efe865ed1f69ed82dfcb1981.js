9c8e802708511da207edcdea9ebaaa7909f51c0c
Woojoung <chae3229@ipixelcorp.com>
2020-06-15 09:24:41 +0900

    Updates

---
+++ precache-manifest.d9066d87efe865ed1f69ed82dfcb1981.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a73eb021374139003f3b195fabcb127",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "2a3de77868b8acc6a212",
+    "url": "/movie_app_2020/static/css/main.fd65f92c.chunk.css"
+  },
+  {
+    "revision": "89018a9f4d0393aaa4a7",
+    "url": "/movie_app_2020/static/js/2.6fa82bcd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.6fa82bcd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a3de77868b8acc6a212",
+    "url": "/movie_app_2020/static/js/main.371b2358.chunk.js"
+  },
+  {
+    "revision": "86e4940ccaeed0f37e85",
+    "url": "/movie_app_2020/static/js/runtime-main.83f6590a.js"
+  }
+]);
